1fc74a955c2afb08a46122a76706cc0076e17d47
Andrey Barbolin <abarbolin@axilon.ru>
2014-09-05 10:16:21 +0400

    first project commit

---
+++ SiteParse/SiteParse/ParseForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SiteParse
+{
+    public partial class ParseForm : Form
+    {
+        public ParseForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
